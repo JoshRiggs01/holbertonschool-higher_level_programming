1d470bfe16fc0f887a703659031747afaaf220c3
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-10 16:31:20 +0000

    fixed newlline

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -9,8 +9,8 @@ else:
 
 print("Last digit of {} is {} ".format(number, last_digit), end="")
 if last_digit > 5:
-    print("and is greater than 5\n")
+    print("and is greater than 5")
 elif last_digit == 0:
-    print("and is 0\n")
+    print("and is 0")
 else:
-    print("and is less than 6 and not 0\n")
+    print("and is less than 6 and not 0")
