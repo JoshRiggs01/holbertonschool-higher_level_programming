9492fe7c403bd2a7c7ea1ee0ab16c3f15bd097c6
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-10 16:55:34 +0000

    pep8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def uppercase(s):
-    output = ''.join([chr(ord(i) - 32) if ord(i) in range(97, 123) else i for i in s])
+    output = ''.join([chr(ord(i) - 32) if ord(i)
+                      in range(97, 123) else i for i in s])
     print("{}".format(output))
