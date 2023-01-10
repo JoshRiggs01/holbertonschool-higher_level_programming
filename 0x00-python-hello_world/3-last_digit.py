33dc4d02dc6bfc7d128fdb630d46f04cb85a4a59
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-10 16:26:11 +0000

    removing newline

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -6,8 +6,8 @@ last_digit = abs(number) % 10
 
 print("Last digit of {} is {} ".format(number, last_digit), end="")
 if last_digit > 5:
-    print("and is greater than 5\n")
+    print("and is greater than 5")
 elif last_digit == 0:
-    print("and is 0\n")
+    print("and is 0")
 else:
-    print("and is less than 6 and not 0\n")
+    print("and is less than 6 and not 0")
