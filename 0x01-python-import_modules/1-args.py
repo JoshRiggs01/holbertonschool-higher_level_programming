4d0f47229292bc89ab0f63eb7428579a5b401daa
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-10 17:26:20 +0000

    naem and main

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argv = sys.argv[1:]
-n = len(argv)
+    argv = sys.argv[1:]
+    n = len(argv)
 
-if n == 0:
-    print("Number of argument(s): {}.".format(n))
-else:
-    print("Number of argument(s): {}:".format(n))
-    for i in range(n):
-        print("{}: {}".format(i + 1, argv[i]))
+    if n == 0:
+        print("Number of argument(s): {}.".format(n))
+    else:
+        print("Number of argument(s): {}:".format(n))
+        for i in range(n):
+            print("{}: {}".format(i + 1, argv[i]))
