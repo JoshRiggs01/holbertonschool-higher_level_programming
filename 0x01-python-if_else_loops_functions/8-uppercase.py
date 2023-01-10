039f67c4bdb2ae79e671279c11099901f8b9c8b4
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-10 16:54:27 +0000

    no new line?

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def uppercase(s):
     output = ''.join([chr(ord(i) - 32) if ord(i) in range(97, 123) else i for i in s])
-    print("{}\n".format(output))
+    print("{}".format(output))
