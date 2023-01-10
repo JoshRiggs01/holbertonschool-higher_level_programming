3b826c1d1c871b4e4f82a0003d2c50a658ff36e4
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-10 17:25:06 +0000

    number of args

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+argv = sys.argv[1:]
+n = len(argv)
+
+if n == 0:
+    print("Number of argument(s): {}.".format(n))
+else:
+    print("Number of argument(s): {}:".format(n))
+    for i in range(n):
+        print("{}: {}".format(i + 1, argv[i]))
