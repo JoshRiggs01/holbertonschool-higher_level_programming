9b0732d80128a8cb9df7729c77de5e470ccac327
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-11 20:08:49 +0000

    0 args 1 arg 2 args

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     n = len(argv)
     if n == 0:
-        print("{} argument:".format(n+1))
-        print("1: {}".format(sys.argv[0]))
+        print("{} argument{}.".format(n, "" if n == 1 else "s"))
     else:
-        print("{} arguments:".format(n+1 - (1 if n>0 else 0)))
+        n_args = n + 1 - (1 if n>0 else 0)
+        print("{} argument{}:".format(n_args, "" if n_args == 1 else "s"))
         for i in range(1, n+1):
             print("{}: {}".format(i, sys.argv[i]))
