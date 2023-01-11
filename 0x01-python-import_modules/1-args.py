0f978b1a5712ae8f4754d52e842f5bc8f64c3bfc
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-11 19:59:30 +0000

    subtract one arg from all

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     argv = sys.argv[1:]
     n = len(argv)
-
     if n == 0:
-        print("{} argument:".format(n))
+        print("{} argument:".format(n+1))
+        print("1: {}".format(sys.argv[0]))
     else:
-        print("{} arguments:".format(n))
-        for i in range(n):
-            print("{}: {}".format(i + 1, argv[i]))
+        print("{} arguments:".format(n+1 - (1 if n>0 else 0)))
+        for i in range(1, n+1):
+            print("{}: {}".format(i, sys.argv[i]))
