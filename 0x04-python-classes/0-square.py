1ba9a1056481c34e8267adc428f35e01a6e4176a
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-18 15:56:34 +0000

    fixing indention

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-    """ Class Square that defines a square by:                                               
-    Private instance attribute: size                                                         
-    Instantiation with size (no type/value verification)
-    """
+""" Class Square that defines a square by:
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+"""
 class Square:
+
+
         """ Initialize class with size """
-    def __init__(self, size):
-        self.__size = size
+        def __init__(self, size):
+                self.__size = size
