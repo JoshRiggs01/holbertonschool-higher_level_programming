1f484e8c3aea3b91749bcdd2f4a2d27bf1d84862
JoshRiggs01 <5574@holbertonstudents.com>
2023-01-18 18:14:11 +0000

    3 sq

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -16,25 +16,25 @@ class Square:
         self.size = size
 
         @property
-        def size(self):
-            """
-            Gets the current size of the square.
-            """
-            return self.__size
+    def size(self):
+        """
+        Gets the current size of the square.
+        """
+        return self.__size
 
-            @size.setter
-            def size(self, value):
-                """
-                Sets the size of the square.
-                """
-                if type(value) != int:
-                    raise TypeError("size must be an integer")
-                    if value < 0:
-                        raise ValueError("size must be >= 0")
-                        self.__size = value
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-                        def area(self):
-                            """
-                            Returns the current square area.
-                            """
-                            return self.__size ** 2
+    def area(self):
+        """
+        Returns the current square area.
+        """
+        return self.__size ** 2
