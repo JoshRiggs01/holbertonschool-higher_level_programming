8490ffd2b858522b381ee68507487c1cf520569a
JoshRiggs01 <5574@holbertonstudents.com>
2023-02-01 04:02:13 +0000

    trying again

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -13,5 +13,5 @@
 
 def write_file(filename="", text=""):
     """ write string to a file """
-    with open(filename, mode="w", encoding='utf-8') as file:
-        return (file.write(text))doesndon
+    with open(filename, 'w', encoding='utf8') as file:
+        return file.write(text)
