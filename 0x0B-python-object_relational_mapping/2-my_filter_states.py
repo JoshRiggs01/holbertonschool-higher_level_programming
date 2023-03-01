3a7b185636ed616dfd6d549b070f19b0f5bb201f
JoshRiggs01 <5574@holbertonstudents.com>
2023-03-01 04:27:47 +0000

    adding 2 filter states

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument
+Write a script that takes in an argument and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
-
-import MySQLdb
 import sys
+import MySQLdb
 
 
-if __name__ == '__main__':
-    # Database connection
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    # Cursor class
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
     cur = conn.cursor()
-
-    # Query
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
-
-    # Results
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-
-    # Close cursor and database connection
+    search = sys.argv[4]
+    query = """SELECT * FROM states where name = '{:s}'
+            ORDER by id ASC""".format(search)
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        if r[1] == search:
+            print(r)
     cur.close()
     conn.close()
+
+
+if __name__ == "__main__":
+    main()
