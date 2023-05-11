b14a5a77ddeab7556aeff404a3da8ab96a793e9a
root <5574@holbertonstudents.com>
2023-05-11 09:07:58 -0700

    adding 3 print number

--- python-hello_world/5-print_comb2.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-output = ''
-for i in range(100):
-    output += "{:02}, ".format(i) if i != 99 else "{:02}\n".format(i)
-print(output, end='')
--- python-hello_world/6-print_comb3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-output = ''
-for i in range(10):
-    for j in range(10):
-        if i != j:
-            output += "{}{}, ".format(i, j) if i < j else "{}{}, ".format(j, i)
-print(output[:-2]+'\n', end='')
