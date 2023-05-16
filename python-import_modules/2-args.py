a27d86eb716e17f17de9ea7e47271ec21c8b0662
root <5574@holbertonstudents.com>
2023-05-16 16:58:07 -0700

    idk

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+        return
+    else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
+
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    n = len(argv)
-    if n == 0:
-        print("{} argument{}.".format(n, "" if n == 1 else "s"))
-    else:
-        n_args = n + 1 - (1 if n>0 else 0)
-        print("{} argument{}:".format(n_args, "" if n_args == 1 else "s"))
-        for i in range(1, n+1):
-            print("{}: {}".format(i, sys.argv[i]))
+    print_arg(sys.argv)
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -13,5 +13,6 @@ def add_arg(argv):
         print("{:d}".format(add))
 
 if __name__ == "__main__":
+
     import sys
     add_arg(sys.argv)
