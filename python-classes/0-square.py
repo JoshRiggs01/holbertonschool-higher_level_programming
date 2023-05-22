d5fbd811da1060fc2d52d57bd0c6b2f2bc520102
root <5574@holbertonstudents.com>
2023-05-22 10:43:51 -0700

    did this fix it >

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ defines the class square"""
+   class Square:
+       pass
 
-
-class Square:
+"""class Square:
     """represents a square"""
 
     def __init__(self, size=0):
         """create new field/private instance size"""
-        self.__size = size
+        self.__size = size"""
