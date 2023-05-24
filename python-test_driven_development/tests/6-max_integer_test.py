ea83aa106c2af000f317def0ccd92c0e6e7ff042
root <5574@holbertonstudents.com>
2023-05-23 18:44:54 -0700

    git init

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,39 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-tests for 6-max_integer
-"""
-
-
 import unittest
-max_integer = __import__('6-max_integer').max_integer
-
+from max_integer import max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    """ tests for max_integer"""
-    def test_def(self):
-        """tests normal input"""
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_positive_integers(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_none(self):
-        """tests empty input"""
-        self.assertEqual(max_integer(), None)
+    def test_negative_integers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-    def test_neg(self):
-        """tests negative"""
-        self.assertEqual(max_integer([-1, -2, -3]), -1)
+    def test_duplicate_max(self):
+        self.assertEqual(max_integer([1, 3, 4, 4, 2]), 4)
 
-    def test_neg_pos(self):
-        """tests composed input"""
-        self.assertEqual(max_integer([1, -4, -5, 5, 3]), 5)
+    def test_single_element(self):
+        self.assertEqual(max_integer([2]), 4)
 
-    def test_non_int(self):
-        """tests with non integer input"""
-        with self.assertRaises(TypeError):
-            max_integer([1, '2', 3])
-
-    def test_float(self):
-        """tests with float"""
-        self.assertEqual(max_integer([1, 2, 3.3]), 3.3)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
