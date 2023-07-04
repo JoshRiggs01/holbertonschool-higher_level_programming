9f234501e4d80393baf6279164ffd6b313ee81c3
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 <5574@holbertonstudents.com>
2023-07-04 17:00:35 -0500

    adding 1-create

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,6 @@
+-- SQL script that creates the database hbtn_0c_0 in your MySQL server.
+--  If the database hbtn_0c_0 already exists, your script should not fail.
+-- not allowed to use the SELECT or SHOW statements.
+
+-- Create database if it does not exisit, if it does no errors or failures
+CREATE database IF not exists hbtn_0c_0;
